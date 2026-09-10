db766faea0b69aafa00e7b580e63e63b10c9983e
Klas Jönsson <klas.joensson@gmail.com>
2026-09-10 13:30:07 +0200

    Refactors the display

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/display.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -12,8 +11,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include <lvgl.h>
 #include "joystick.h"
+#include "display.h"
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 
@@ -22,23 +21,13 @@ LOG_MODULE_REGISTER(main);
 static uint32_t time_count;
 static uint32_t hit_count = 0;
 static int32_t offset[2] = {0, 0};
-static int has_food = 0;
 static int is_hunter_active = 0;
-lv_obj_t *food = NULL;
-lv_obj_t *hunter = NULL;
+int has_food = 0;
 // Food coordinates let's say the food is at (1000, 1000) which is outside the display when not displayed.
 static int food_coordinates[2] = {1000, 1000};
 static int hunter_coordinates[2] = {1000, 1000};
 static int use_callback = 1;
 static int fire = 0;
-
-char count_str[11] = {0};
-char info_str[21] = {0};	
-static lv_style_t end_txt_style;
-static lv_style_t start_txt_style;
-lv_obj_t *count_label;
-lv_obj_t *info_label;
-lv_obj_t *player;
 
 static struct gpio_dt_spec mode_button = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
 static struct gpio_dt_spec fire_button = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw1), gpios, {0});
@@ -103,32 +92,27 @@ static void fire_button_callback(const struct device *port,
 	fire = 1;
 }	
 
-
 static void show_food() {
-	if (food == NULL) {
-		has_food = 1;
-		food = lv_obj_create(lv_screen_active());
-		lv_obj_set_size(food, 15, 15);
-		lv_obj_set_style_radius(food, LV_RADIUS_CIRCLE, LV_PART_MAIN);
-	}
 	int  y = rand() % 241 - 120; // Random y offset between -120 and 120
 	int max_offset = (int)(sqrt(14400 - pow(y, 2)));
-	int x = rand() % (2 * max_offset + 1) - max_offset; // Random x offset based on y 
-	lv_obj_align(food, LV_ALIGN_CENTER, x, y);
+	int x = rand() % (2 * max_offset + 1) - max_offset; // Random x offset based on y
 	food_coordinates[0] = x;
 	food_coordinates[1] = y;
+	add_food(x, y);
 	LOG_INF("New food at [%d, %d]", x, y);
 }
 
 static int check_collision() {
 	int dx;
 	int dy;
-	if (food_coordinates[0] == 1000 && !(hunter_coordinates[0] > 500)) {
+	if (is_hunter_active) {
 		dx = offset[0] - hunter_coordinates[0];
 		dy = offset[1] - hunter_coordinates[1];
-	} else {
+	} else if (has_food) {
 		dx = offset[0] - food_coordinates[0];
 		dy = offset[1] - food_coordinates[1];
+	} else {
+		return 0; // No collision if neither hunter nor food is active
 	}
 	int distance_squared = dx * dx + dy * dy;
 	if (distance_squared <= 100) { // 10^2 = 100
@@ -139,38 +123,12 @@ static int check_collision() {
 	
 }
 
-// Move new player out of view when it is not displayed
-static void hide_food() {
-	if (has_food) {
-		food_coordinates[0] = 1000; // Move food coordinates out of view
-		food_coordinates[1] = 1000;
-		lv_obj_align(food, LV_ALIGN_CENTER, food_coordinates[0], food_coordinates[1]);
-		LOG_INF("Food hidden");
-	}
-}
-
 static void create_hunter() {
-	// create a square at the same place as the player to represent the hunter
-	if (hunter == NULL) {
-		hunter = lv_obj_create(lv_screen_active());
-		lv_obj_set_size(hunter, 20, 20);
-		lv_obj_set_style_radius(hunter, 0, LV_PART_MAIN);
-	} 
 	hunter_coordinates[0] = food_coordinates[0];
 	hunter_coordinates[1] = food_coordinates[1];
-	hide_food();
+	remove_food();
+	add_hunter(hunter_coordinates[0], hunter_coordinates[1]);
 	is_hunter_active = 1;
-	lv_obj_align(hunter, LV_ALIGN_CENTER, hunter_coordinates[0], hunter_coordinates[1]);
-}
-
-static void remove_hunter() {
-	if (hunter != NULL) {
-		lv_obj_del(hunter);
-		hunter = NULL;
-	}
-	is_hunter_active = 0;
-	hunter_coordinates[0] = 1000; // Move hunter coordinates out of view
-	hunter_coordinates[1] = 1000;
 }
 
 static int configure_mode_button() {
@@ -205,6 +163,7 @@ static int configure_mode_button() {
 	return 0;
 }
 
+//keep in main
 static int configure_fire_button() {
 	int err;
 	if (!gpio_is_ready_dt(&fire_button)) {
@@ -239,22 +198,6 @@ static int configure_fire_button() {
 
 static int configure_device() {
 	int ret;
-	const struct device *display_dev;
-	/* Check if the display device is ready	 */
-	display_dev =  DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
-	if (!device_is_ready(display_dev)) {
-		LOG_ERR("Display device is not ready");
-		return -1;
-	}
-	/* Turn off display blanking to ensure the display is active
-	 * Check the return value and if it is less than 0 and not equal to -ENOSYS,
-	 * log an error message and return 0
-	 */
-	ret = display_blanking_off(display_dev);
-	if (ret < 0 && ret != -ENOSYS) {
-		LOG_ERR("Failed to turn blanking off (error %d)", ret);
-		return -1;
-	}
 
 	/*configure mode btn*/
 	ret = configure_mode_button();
@@ -278,61 +221,45 @@ static void move_player() {
 	int pin_status = use_callback ? get_status() : get_pin_status();
 	if (pin_status != 0) {
 		LOG_INF("Pin status: %d", pin_status);
+		if (pin_status & 4) { 
+			move_player_left();
+		}
+		if (pin_status & 8) { 
+			move_player_right();
+		}
+		if (pin_status & 1) { 
+			move_player_up();
+		}
+		if (pin_status & 2) { 
+			move_player_down();
+		}
+		if (pin_status & 16) { 
+			return;
+		}
+		update_player_position(offset[0], offset[1]);
 	}
-	
-	if (pin_status & 4) { 
-		move_player_left();
-	}
-	if (pin_status & 8) { 
-		move_player_right();
-	}
-	if (pin_status & 1) { 
-		move_player_up();
-	}
-	if (pin_status & 2) { 
-		move_player_down();
-	}
-	lv_obj_align(player, LV_ALIGN_CENTER, offset[0], offset[1]);	
 }
 
 static void show_start_screen() {
-	hit_count = 0;
-	info_label = lv_label_create(lv_screen_active());
-	lv_obj_add_style(info_label, &start_txt_style, LV_STATE_DEFAULT);
-	lv_label_set_text(info_label, "Press fire to start");
-	lv_obj_align(info_label, LV_ALIGN_CENTER, 0, 0);
-	lv_timer_handler();
+	show_info_label("Press fire to start");
 	LOG_INF("Start screen up, waiting for button press");
 	while (1) {
-		if (is_button_pressed()) {
-			lv_obj_del(info_label);
-			start_game();
-		} else if (fire) {
+		if (is_button_pressed() || fire) {
 			fire = 0;
-			lv_obj_del(info_label);
 			start_game();
-		}
+		} 
 	}
 }
 
 static void end_game() {
-	lv_obj_del(player);
-
-	info_label = lv_label_create(lv_screen_active());
-	lv_obj_add_style(info_label, &end_txt_style, LV_STATE_DEFAULT);
-	lv_label_set_text(info_label, "GAME OVER");
-	lv_obj_align(info_label, LV_ALIGN_CENTER, 0, 0);
-	lv_timer_handler();
+	remove_player();
+	show_info_label("GAME OVER");
 	LOG_INF("Game over!");
 	while (1) {
-		if (is_button_pressed()) {
-			lv_obj_del(info_label);
-			lv_obj_del(count_label);
-			show_start_screen();
-		} else if (fire) {
+		if (is_button_pressed() || fire) {
 			fire = 0;
-			lv_obj_del(info_label);
-			lv_obj_del(count_label);
+			remove_info_label();
+			remove_count_label();
 			show_start_screen();
 		}
 	}
@@ -343,30 +270,40 @@ static void play_game() {
 	int hunter_time = 750;
 	LOG_INF("Let the game begin...");
 	while (1) {
-		move_player();
 		/* Increment the time count */
 		++time_count;
+		move_player();
 		if (time_count == food_time) {
 			show_food();
+			has_food = 1;
 			LOG_INF("New food created at [%d, %d]", food_coordinates[0], food_coordinates[1]);
 		} else if (time_count == hunter_time) {
+				remove_food();
+				has_food = 0;
 				create_hunter();
+				is_hunter_active = 1;
 				LOG_INF("Hunter created at [%d, %d]", hunter_coordinates[0], hunter_coordinates[1]);
 		} else if (time_count == 1250) {
 				remove_hunter();
 				time_count = 0;
+				is_hunter_active = 0;
 				LOG_INF("Hunter removed");
 		} else {
 			if (check_collision()) {			
 				if (is_hunter_active) {
 					LOG_INF("Collision detected when on [%d, %d] with hunter at [%d, %d]", offset[0], offset[1], hunter_coordinates[0], hunter_coordinates[1]);
 					remove_hunter();
+					hunter_coordinates[0] = 1000;
+					hunter_coordinates[1] = 1000;
 					time_count = 0;
 					end_game();
-				} else {
+				}
+				if (has_food) {
 					LOG_INF("Collision detected when on [%d, %d] with food at [%d, %d]", offset[0], offset[1], food_coordinates[0], food_coordinates[1]);
-					hide_food();
-					hit_count++;
+					remove_food();
+					food_coordinates[0] = 1000;
+					food_coordinates[1] = 1000;
+					update_count_label(++hit_count);
 					time_count = 0;
 					if (hit_count%5 == 0) {
 						food_time = (food_time>0)? (food_time-50): 1;
@@ -387,38 +324,40 @@ static void play_game() {
 						hunter_coordinates[1]--;
 					}
 				}
-				lv_obj_align(hunter, LV_ALIGN_CENTER, hunter_coordinates[0], hunter_coordinates[1]);
+				update_hunter_position(hunter_coordinates[0], hunter_coordinates[1]);
 			} 
 		}
-		sprintf(count_str, "%d", hit_count);
-		lv_label_set_text(count_label, count_str);
-		lv_timer_handler();
+		
 		k_sleep(K_USEC(9501));
 	}
 }
 
-static void start_game() {
-	srand(time(NULL));
-    /* Create a label to display time count and align it at the bottom center */
-	count_label = lv_label_create(lv_screen_active());
-	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+static void reset_game_var() {
+	hit_count = 0;
+	time_count = 0;
+	has_food = 0;
+	is_hunter_active = 0;
+	offset[0] = 0;
+	offset[1] = 0;
+}
 
-	/* Draw a 25px diameter circle in the middle of the display to represent the player */
-	player = lv_obj_create(lv_screen_active());
-	lv_obj_set_size(player, 25, 25);
-	lv_obj_set_style_radius(player, LV_RADIUS_CIRCLE, LV_PART_MAIN);
-	lv_obj_align(player, LV_ALIGN_CENTER, 0, 0);
-	sprintf(count_str, "%d", hit_count);
-	lv_label_set_text(count_label, count_str);
-	/* To update the display, call the LVGL timer handler */
-	lv_timer_handler();
+static void start_game() {
+	reset_game_var();
+	srand(time(NULL));
+	remove_info_label();
+	create_player();
+	update_count_label(hit_count);
 	play_game();
 }
 
-
-
 int main(void) {
-	int ret = configure_device();
+	int ret = configure_display();
+	if (ret < 0) {
+		LOG_ERR("Failed to configure display");
+		return -1;
+	}
+
+	ret = configure_device();
 	if (ret < 0) {
 		LOG_ERR("Could not configure the device");
 		return -1;
@@ -430,11 +369,7 @@ int main(void) {
 		return -1;
 	}
 
-	lv_style_init(&end_txt_style);
-	lv_style_set_text_font(&end_txt_style, &lv_font_montserrat_28);
-	lv_style_init(&start_txt_style);
-	lv_style_set_text_font(&start_txt_style, &lv_font_montserrat_22);
-	
+	init_display();
 	show_start_screen();
 
 	return 0;
